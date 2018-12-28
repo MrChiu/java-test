f6372cc8eb03d4d3c0984b779591d5336d792b4b
Leviathan <qiudong0396@qq.com>
2018-12-28 14:22:14 +0800

    。

---
+++ src/main/java/com/example/reactorTest/FluxTest.java
@@ -0,0 +1,48 @@
+package com.example.reactorTest;
+
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author: qiudong
+ * @description:
+ * @date: Created in 10:42 2018-12-26
+ */
+public class FluxTest {
+
+    public static void main(String[] args){
+//        Flux.just("Hello", "World").subscribe(System.out::println);
+//        Flux.fromArray(new Integer[] {1, 2, 3}).subscribe(System.out::println);
+//        Flux.empty().subscribe(System.out::println);
+//        Flux.range(1, 10).subscribe(System.out::println);
+//        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
+
+//        Flux.generate(sink -> {
+//            sink.next("Hello");
+//            sink.complete();
+//        }).subscribe(System.out::println);
+
+
+//        final Random random = new Random();
+//        Flux.generate(ArrayList::new, (list, sink) -> {
+//            int value = random.nextInt(100);
+//            list.add(value);
+//            sink.next(value);
+//            if (list.size() == 10) {
+//                sink.complete();
+//            }
+//            return list;
+//        }).subscribe(System.out::println);
+
+        Flux.create(sink -> {
+            for (int i = 0; i < 10; i++) {
+                sink.next(i);
+            }
+            sink.complete();
+        }).subscribe(System.out::println);
+    }
+}
